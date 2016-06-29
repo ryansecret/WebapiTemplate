1675fc5aa8284127752f676b62c729090b926c72
ryan <shaolinshi111@163.com>
2016-06-29 18:10:25 +0800

    ryan

---
+++ Ryan.Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ryan.Repository
+{
+    public interface IRepository<T,K> where T:class,new() where K:struct 
+    {
+        T FindBy(K key);
+
+        IList<T> FindAll();
+
+        void Add(T item);
+
+        void Update(T item);
+
+        void Remove(T item);
+
+        void RemoveAll();
+    }
+}
---
+++ Ryan.Repository/Utility/Extension.cs
@@ -0,0 +1,15 @@
+﻿using DapperExtensions;
+
+namespace Ryan.Repository.Utility
+{
+    public static class Extension
+    {
+        public static void AddPredicate(this PredicateGroup predicateGroup, bool conditon, IPredicate predicate)
+        {
+            if (conditon)
+            {
+                predicateGroup.Predicates.Add(predicate);
+            }
+        }
+    }
+}
